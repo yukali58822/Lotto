07250a4e6481097998b8bc76729687d79a3168a2
yukali58822 <yukali58822@gmail.com>
2024-03-06 18:03:24 +0800

    按鈕文字為作者樂透

--- app/src/main/java/com/example/yukali/lotto/MainActivity.kt
+++ app/src/main/java/com/example/yukali/lotto/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.yukali.lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var t: TextView = findViewById(R.id.txv)
-        t.text = "app"
+        t.text = "0"
+
+    }
+    fun happy(v: View){
+        var txv: TextView = findViewById(R.id.txv)
+        if (v.id == R.id.btn){
+            txv.text = (1..100).random().toString()
+        }
+        else{
+            txv.text = "0"
+        }
     }
 }
